f8d3ccf3aad9ae74d8794c8805beec72824d1698
gavrilovone <gavrilovone@mail.ru>
2022-04-22 15:44:10 +0400

    home task lesson 9

---
+++ src/homeRaskLesson9/Test1.java
@@ -0,0 +1,21 @@
+
+package homeRaskLesson9;
+/*
+  Вычислите устно output-ы данных программ, а затем проверьте в NetBeans
+*/
+
+public class Test1 {
+    
+    int a = 1;
+   // static int a = 2;
+    void abc(int a){
+        System.out.println(a);
+        System.out.println(this.a);
+    
+    }
+    
+    public static void main(String[] args) {
+        Test1 t =  new Test1();
+        t.abc(3);
+    }
+}
